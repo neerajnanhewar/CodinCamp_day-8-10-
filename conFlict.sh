1a71b1c89243c3c25baad7f90f99b9c371635d02
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-07-16 19:54:27 +0530

    Update conFlict.sh

--- conFlict.sh
+++ conFlict.sh
@@ -5,6 +5,3 @@ echo "hello222"
 <<<<<<< HEAD
 echo "ppppppppp"
 =======
->>>>>>> 18f5d3c8612d104158e2bd3b0b1fcd4472b2c99e
-echo "wwwwwwwwwwwwwwwwww"
->>>>>>> 5bc6b76a1f55bb822829a9e397638b979f7cf36e
